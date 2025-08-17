9e773f96c38d8269fd960ffc67f6941bc6757763
Onkabetse Sebothoma <gosiameonkabetse077@gmail.com>
2025-08-17 14:34:00 +0200

    Working on the seller home

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,23 @@ import {RouterLink} from '@angular/router';
   styleUrl: './header.css'
 })
 export class Header {
+  menuType= 'Default';
+constructor(private route:Router) {
+}
+ngOnInit() {
+  this.route.events.subscribe((data:any) => {
+    if (data.url) {
+      console.warn(data.url);
+      if(localStorage.getItem('seller') && data.url.includes('seller')){
+        console.log('This is Seller Area');
+        this.menuType='Seller';
+      }else{
+        console.warn("Outside Seller Area");
+        this.menuType='Default';
+      }
+    }
 
+  })
+}
 }
 
